e0e2035e3ce5d4efb2575938d87245240f77738e
Marvin ELEGBEDE <marvin.elegbede@gmail.com>
2026-05-13 08:03:13 +0100

    Suppression dossier src/src en double

--- src/src/pages/Incidents.tsx
+++
@@ -1,8 +0,0 @@
-export default function Incidents() {
-  return (
-    <div className="p-8">
-      <h1 className="text-2xl font-bold text-slate-800">Incidents</h1>
-      <p className="text-slate-500 mt-1">En cours de construction...</p>
-    </div>
-  )
-}
